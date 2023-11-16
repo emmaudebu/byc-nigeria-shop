b9c068b251c3c9d3d34ff1ad5f20bfda470ddf6c
ifeanyi chukwu <emmanueludebu@gmail.com>
2023-11-16 11:16:54 +0100

    Hero section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Hero = () => {
-  return <section></section>;
+  return (
+    <section>
+      <div className=" text-center mt-5 md:mt-[96px] md:mx-[210px]">
+        <p className=" mb-3 text-lg  md:mb-10 md:text-2xl font-medium ">
+          {" "}
+          Your Body Deserves Comfort
+        </p>
+        <p className=" text-2xl font-black md:text-6xl">
+          Get the best for yourself
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
